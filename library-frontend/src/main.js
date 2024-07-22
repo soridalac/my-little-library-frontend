060a693c82ee428269082b0d490f9ac196b7884c
sorida lac <lacsorida@gmail.com>
2024-07-22 15:43:06 -0700

    import bootstrap into main.js

--- library-frontend/src/main.js
+++ library-frontend/src/main.js
@@ -1,8 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.main.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 const app = createApp(App)
 
