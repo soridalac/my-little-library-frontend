f87584b21d2e91d218d77e4ee49d21ecbcb31f52
sorida lac <lacsorida@gmail.com>
2024-07-24 11:23:57 -0700

    add post and delete methods

---
+++ library-frontend/src/views/AddBook.vue
@@ -0,0 +1,126 @@
+<template>
+    <main>
+        <Navbar />
+        <div class="my-5">
+            <div class="mx-auto w-25 " style="max-width:100%;">
+                <h2 class="text-center mb-3">Add Book</h2>
+                <form @submit.prevent="addBook">
+
+                <!--Title-->
+                <div class="row">
+                    <div class="col-md-12 form-group mb-3">
+                    <label for="title" class="form-label">Title</label>
+                    <input id="title"  type="text" name="title" class="form-control" placeholder="Title" required v-model="book.title">
+                    </div>
+                </div>
+
+                
+                <!--Author-->
+                <div class="row">
+                    <div class="col-md-12 form-group mb-3">
+                        <label for="author" class="form-label">Author</label>
+                        <input id="author" type="text"  name="author" class="form-control" placeholder="Author" required v-model="book.author" >
+                    </div>
+                    </div>
+
+                <!--Description-->
+                <div class="row">
+                    <div class="col-md-12 form-group mb-3">
+                        <label for="description" class="form-label">Description</label>
+                        <input id="description" type="text"  name="description" class="form-control" placeholder="Description" required v-model="book.description" >
+                    </div>
+                    </div>
+                
+                <!--Published Year-->
+                <div class="row">
+                    <div class="col-md-12 form-group mb-3">
+                        <label for="published_year" class="form-label">Published Year</label>
+                        <input id="published_year" type="published_year"  name="published_year" class="form-control" placeholder="published_year" required v-model="book.published_year" >
+                    </div>
+                    </div>
+
+                <!--Language-->
+                <div class="row">
+                    <div class="col-md-12 form-group mb-3">
+                        <label for="language" class="form-label">Language</label>
+                        <input id="language" type="text"  name="language" class="form-control" placeholder="Language" required v-model="book.language" >
+                    </div>
+                    </div>
+
+                <!--Genre-->
+                <label for="genre" class="form-label">Genre</label>
+                <div class="form-check">
+                    <input class="form-check-input"   type="radio" name="genre" id="fantasy" value="fantasy" v-model="book.genre">
+                    <label class="form-check-label" for="fantasy">Fantasy</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio"   name="genre" id="romance" value="romance" v-model="book.genre">
+                    <label class="form-check-label" for="romance">Romance</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio"   name="genre" id="others" checked value="others" v-model="book.genre">
+                    <label class="form-check-label" for="others">Others</label>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-12 form-group">
+                    <input class="btn btn-primary w-100" type="submit" value="Submit">
+                    </div>
+                </div>
+
+                <div>
+            
+                </div>
+                </form>
+            </div>
+        </div>
+    </main>
+</template>
+
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+
+    export default {
+        name: 'AddBook',
+        components: {
+            Navbar
+        },
+
+        data() {
+            return {
+                book : {
+                    title: '',
+                    author: '',
+                    genre: '',
+                    description: '',
+                    published_year: '',
+                    language: '',
+
+                }
+            }
+        },
+
+        methods: {
+            addBook(){
+                fetch('http://localhost:8080/api/books', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.book)
+                })
+                .then(data => {
+                    console.log(data)
+                    this.$router.push("/");
+                })
+
+            }
+        },
+            
+    }
+
+
+
+</script>
---
+++ library-frontend/src/views/ViewBooks.vue
@@ -0,0 +1,88 @@
+<template>
+    <main>
+        <Navbar />
+
+        <!-- Table-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12"> 
+                    <h1 class="text-center">View Book</h1>
+                    <!--Add button -->
+                    <a href="/api/books" class="btn btn-primary">Add Book</a>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Published Year</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Language</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="book in books" :key="book.id">
+                            <th scope="row">{{book.id}}</th>
+                            <td>{{book.title}}</td>
+                            <td>{{book.author}}</td>
+                            <td>{{book.description}}</td>
+                            <td>{{book.published_year}}</td>
+                            <td>{{book.genre}}</td>
+                            <td>{{book.language}}</td>
+                            <td>
+                                <a class="btn btn-primary" :href="`/edit/${book.id}`">Edit</a>
+                                <button class="btn btn-danger mx-2" @click="deleteBook(book.id)">Delete</button>
+                            </td>
+                            </tr>
+                        </tbody>
+                        </table>
+                </div>
+            </div>
+        </div>
+        
+    </main>
+</template>
+
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+    export default {
+        name: 'ViewBooks',
+        components: {
+            Navbar
+        },
+        data() {
+            return {
+                books: []
+            }
+        },
+
+        beforeMount(){
+            this.getBooks()
+        },
+
+        methods: {
+            getBooks(){
+                fetch('http://localhost:8080/api/books')
+                .then(res => res.json())
+                .then(data => {
+                    this.books = data
+                    console.log(data)
+                })
+            },
+            deleteBook(id){
+                fetch(`http://localhost:8080/api/books/${id}`, {
+                    method: 'DELETE'
+                })
+                .then(data => {
+                    console.log(data)
+                    this.getBooks()
+                })
+            }
+        }
+
+    }
+
+</script>
