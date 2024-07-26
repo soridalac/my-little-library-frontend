8c1561964bf2ef75c08aff40a9df97eb32893143
Brittany <brittanyclemons.info@gmail.com>
2024-07-25 17:57:17 -0700

    search button

--- library-frontend/src/views/ViewBooks.vue
+++ library-frontend/src/views/ViewBooks.vue
@@ -5,20 +5,21 @@
         <!-- Table-->
         <div class="container">
             <div class="row">
-                <div class="col-md-12"> 
+                <div class="col-md-12">
                     <h1 class="text-center">View Book</h1>
                     <!--Add button -->
+                    <button @click="search">SEARCH</button>
                     <a href="/api/books" class="btn btn-primary">Add Book</a>
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Author</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Published Year</th>
-                            <th scope="col">Genre</th>
-                            <th scope="col">Language</th>
+                                <th scope="col">Id</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Author</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Published Year</th>
+                                <th scope="col">Genre</th>
+                                <th scope="col">Language</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -30,59 +31,68 @@
                             <td>{{book.published_year}}</td>
                             <td>{{book.genre}}</td>
                             <td>{{book.language}}</td>
-                            <td>
-                                <a class="btn btn-primary" :href="`/edit/${book.id}`">Edit</a>
-                                <button class="btn btn-danger mx-2" @click="deleteBook(book.id)">Delete</button>
-                            </td>
+                                <td>
+                                    <a class="btn btn-primary" :href="`/edit/${book.id}`">Edit</a>
+                                    <button class="btn btn-danger mx-2" @click="deleteBook(book.id)">Delete</button>
+                                </td>
                             </tr>
                         </tbody>
-                        </table>
+                    </table>
                 </div>
             </div>
         </div>
-        
+
     </main>
 </template>
 
 
 <script>
+import { searchBooks } from '@/services/bookList';
 import Navbar from '../components/Navbar.vue'
 
-    export default {
-        name: 'ViewBooks',
-        components: {
-            Navbar
-        },
-        data() {
-            return {
-                books: []
-            }
-        },
+export default {
+    name: 'ViewBooks',
+    components: {
+        Navbar
+    },
+    data() {
+        return {
+            books: []
+        }
+    },
 
         beforeMount(){
-            this.getBooks()
-        },
+        this.getBooks()
+    },
 
-        methods: {
+    methods: {
             getBooks(){
-                fetch('http://localhost:8080/api/books')
+            fetch('http://localhost:8080/api/books')
                 .then(res => res.json())
                 .then(data => {
                     this.books = data
                     console.log(data)
                 })
-            },
+        },
             deleteBook(id){
-                fetch(`http://localhost:8080/api/books/${id}`, {
-                    method: 'DELETE'
-                })
+            fetch(`http://localhost:8080/api/books/${id}`, {
+                method: 'DELETE'
+            })
                 .then(data => {
                     console.log(data)
                     this.getBooks()
                 })
+        }, 
+        async search(event) {
+            const result = await searchBooks('test');
+            alert(result);
+            // `event` is the native DOM event
+            if (event) {
+                alert(event.target.tagName)
             }
-        }
-
+        },
     }
+
+}
 
 </script>
